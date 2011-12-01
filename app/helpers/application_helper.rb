05935b99a4a1cc1b77431821cd2410758f046907
tim-tang <tang.jilong@139.com>
2011-12-01 16:05:49 +0800

    highlight nav tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def highlight_home
+    if session[:category]==0
+      "active"
+    end
+  end
+
+  def highlight_nav(cate)
+    if session[:category]== cate.category
+      "active"
+    else
+      ""
+    end
+  end
 end
